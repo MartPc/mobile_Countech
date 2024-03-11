5d85def2d42200fab0323695f8f5f7bb7a24b444
MartPc <mpinzonc07@gmail.com>
2024-03-11 17:33:08 -0500

    Agrega logo y estilo

--- lib/presentation/widgets/app_bar.dart
+++ lib/presentation/widgets/app_bar.dart
@@ -6,26 +6,35 @@ class AppMenuBar extends StatelessWidget implements PreferredSizeWidget{
 
   @override
   Widget build(BuildContext context) {
-    return AppBar(
-      title: const Text("Countech",style: TextStyle(
-        fontSize: 23,
-        fontFamily: 'Caprismo',
-        color: Color.fromARGB(255, 255, 255, 255),
-      ),
-      ),
-      centerTitle: true,
-      backgroundColor:Color.fromARGB(255, 68, 68, 68),
-      automaticallyImplyLeading: false,
-      actions: [
-        IconButton(
-          icon: Icon(Icons.logout),
-          color: Color.fromARGB(255, 255, 255, 255),
-          onPressed: () {
-            Navigator.push(context, MaterialPageRoute(builder: (context) => const LoginScreen()));
-          },
-        ),
-      ]
-          );
+return AppBar(
+  title: const Text("Countech",style: TextStyle(
+    fontSize: 23,
+    fontFamily: 'Caprismo',
+    color: Color.fromARGB(255, 255, 255, 255),
+  ),
+  ),
+  centerTitle: true,
+  backgroundColor:Color.fromARGB(255, 68, 68, 68),
+  automaticallyImplyLeading: false,
+  leading: Padding(
+    padding: EdgeInsets.only(left: 16), // Ajusta el valor según sea necesario
+    child: Image.asset(
+      'assets/logo_countech.png',
+      width: 40,
+      height: 40,
+      fit: BoxFit.contain,
+    ),
+  ),
+  actions: [
+    IconButton(
+      icon: Icon(Icons.logout),
+      color: Color.fromARGB(255, 255, 255, 255),
+      onPressed: () {
+        Navigator.push(context, MaterialPageRoute(builder: (context) => const LoginScreen()));
+      },
+    ),
+  ]
+);
   }
   
   @override
