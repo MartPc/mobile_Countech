f3e03e5db6bbf7f8254b2f31c24df490e0619871
PaulaNino <paulaa_ninob@soy.sea.edu.co>
2024-03-14 07:02:17 -0500

    login

--- lib/presentation/views/login/login_screen.dart
+++ lib/presentation/views/login/login_screen.dart
@@ -1,48 +1,109 @@
 import 'package:countech_app/presentation/screens/main_screen.dart';
 import 'package:flutter/material.dart';
 
-
 class LoginScreen extends StatefulWidget {
-  const LoginScreen({super.key});
+  const LoginScreen({Key? key}) : super(key: key);
 
   @override
   State<LoginScreen> createState() => _LoginScreenState();
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  bool _obscureText = true;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Login'),
-        automaticallyImplyLeading: false,
-
-      ),
+      
       body: Center(
         child: Padding(
           padding: const EdgeInsets.all(20.0),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              const TextField(
-                decoration: InputDecoration(
-                  hintText: 'Correo electrónico',
+              Image.asset(
+                    'assets/logo_countech1.png', // Ruta de tu imagen
+                    height: 150.0, // Ajusta la altura según sea necesario
+                    width: 150.0, // Ajusta el ancho según sea necesario
+                    // Puedes ajustar más propiedades según tus necesidades
+                  ),
+                  const SizedBox(width: 10.0), // Espacio entre la imagen y el texto
+                  
+              const Text(
+                'Iniciar Sesión',
+                style: TextStyle(
+                  fontSize: 24.0,
+                  fontWeight: FontWeight.w900,
                 ),
               ),
               const SizedBox(height: 20.0),
-              const TextField(
+              TextField(
+                decoration: InputDecoration(
+                  hintText: 'Email',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30.0),
+                    borderSide: const BorderSide(color: Colors.red), // Cambia el color del borde a rojo
+                  ),
+                  filled: true,
+                  fillColor: const Color.fromARGB(255, 255, 255, 255),
+                  prefixIcon: Icon(
+                    Icons.email,
+                    color: Colors.grey[400], // Cambia el color del icono a gris más claro
+                  ),
+                  contentPadding: EdgeInsets.symmetric(vertical: 12.0),
+                ),
+              ),
+              const SizedBox(height: 20.0),
+              TextField(
                 decoration: InputDecoration(
                   hintText: 'Contraseña',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30.0),
+                    borderSide: const BorderSide(color: Colors.red), // Cambia el color del borde a rojo
+                  ),
+                  filled: true,
+                  fillColor: Color.fromARGB(255, 255, 255, 255),
+                  prefixIcon: Icon(
+                    Icons.lock,
+                    color: Colors.grey[400], // Cambia el color del icono a gris más claro
+                  ),
+                  suffixIcon: IconButton(
+                    icon: Icon(Icons.visibility),
+                    color: Colors.grey[400], // Cambia el color del icono a gris más claro
+
+                    onPressed: () {
+                      setState(() {
+                        _obscureText = !_obscureText;
+                      });
+                    },
+                  ),
+                  contentPadding: EdgeInsets.symmetric(vertical: 12.0),
                 ),
-                obscureText: true,
+                obscureText: _obscureText,
               ),
               const SizedBox(height: 20.0),
               ElevatedButton(
                 onPressed: () {
-                  Navigator.push(context, MaterialPageRoute(builder: (context) => const MainScreen()));
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const MainScreen()),
+                  );
                 },
-                child: const Text('Iniciar sesión'),
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all<Color>(Colors.blue),
+                  minimumSize: MaterialStateProperty.all<Size>(Size(double.infinity, 52)), // Ajusta el tamaño del botón
+                ),
+                child: Text(
+                  'Ingresar',
+                  style: TextStyle(
+                    color: Colors.white, // Cambia el color del texto a blanco
+                    fontSize: 18.0, // Tamaño del texto más grande
+                    fontWeight: FontWeight.bold, // Texto en negrita
+                  ),
+                ),
               ),
+
+
             ],
           ),
         ),
--- lib/presentation/widgets/app_bar.dart
+++ lib/presentation/widgets/app_bar.dart
@@ -1,44 +1,69 @@
 import 'package:countech_app/presentation/views/login/login_screen.dart';
 import 'package:flutter/material.dart';
 
-class AppMenuBar extends StatelessWidget implements PreferredSizeWidget{
-  const AppMenuBar({super.key});
+class AppMenuBar extends StatelessWidget implements PreferredSizeWidget {
+  const AppMenuBar({Key? key});
 
   @override
   Widget build(BuildContext context) {
-return AppBar(
-  title: const Text("Countech",style: TextStyle(
-    fontSize: 23,
-    fontFamily: 'Caprismo',
-    color: Color.fromARGB(255, 255, 255, 255),
-  ),
-  ),
-  centerTitle: true,
-  backgroundColor:Color.fromARGB(255, 68, 68, 68),
-  automaticallyImplyLeading: false,
-  leading: Padding(
-    padding: EdgeInsets.only(left: 16), // Ajusta el valor según sea necesario
-    child: Image.asset(
-      'assets/logo_countech.png',
-      width: 40,
-      height: 40,
-      fit: BoxFit.contain,
-    ),
-  ),
-  actions: [
-    IconButton(
-      icon: Icon(Icons.logout),
-      color: Color.fromARGB(255, 255, 255, 255),
-      onPressed: () {
-        Navigator.push(context, MaterialPageRoute(builder: (context) => const LoginScreen()));
-      },
-    ),
-  ]
-);
+    return AppBar(
+      title: const Text(
+        "Countech",
+        style: TextStyle(
+          fontSize: 23,
+          fontFamily: 'Caprismo',
+          color: Color.fromARGB(255, 255, 255, 255),
+        ),
+      ),
+      centerTitle: true,
+      backgroundColor: Color.fromARGB(255, 68, 68, 68),
+      automaticallyImplyLeading: false,
+      leading: Padding(
+        padding: EdgeInsets.only(left: 16), // Ajusta el valor según sea necesario
+        child: Image.asset(
+          'assets/logo_countech.png',
+          width: 40,
+          height: 40,
+          fit: BoxFit.contain,
+        ),
+      ),
+      actions: [
+        IconButton(
+          icon: Icon(Icons.logout),
+          color: Color.fromARGB(255, 255, 255, 255),
+          onPressed: () {
+            // Mostrar alerta al presionar el botón de cerrar sesión
+            showDialog(
+              context: context,
+              builder: (BuildContext context) {
+                return AlertDialog(
+                  title: Text('Cerrar Sesión'),
+                  content: Text('¿Está seguro de que desea cerrar sesión?'),
+                  actions: [
+                    TextButton(
+                      onPressed: () {
+                        Navigator.of(context).pop(); // Cerrar la alerta
+                      },
+                      child: Text('Cancelar'),
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        // Aquí puedes añadir la lógica para cerrar la sesión
+                        Navigator.of(context).pop(); // Cerrar la alerta
+                        Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => const LoginScreen())); // Navega a la pantalla de inicio de sesión
+                      },
+                      child: Text('Aceptar'),
+                    ),
+                  ],
+                );
+              },
+            );
+          },
+        ),
+      ],
+    );
   }
-  
-  @override
-  // TODO: implement preferredSize
-Size get preferredSize => Size(0, kToolbarHeight);
 
+  @override
+  Size get preferredSize => Size(0, kToolbarHeight);
 }
