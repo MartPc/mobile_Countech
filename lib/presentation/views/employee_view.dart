767f60c0fffc5838b7dc892fa6de04cead1f39c4
MartPc <mpinzonc07@gmail.com>
2024-03-18 11:25:19 -0500

    Modificacion Empleado

--- lib/presentation/views/employee_view.dart
+++ lib/presentation/views/employee_view.dart
@@ -64,6 +64,8 @@ class _EmployeeViewState extends State<EmployeeView> {
             margin: const EdgeInsets.all(10),
             child: filterItems == null
                 ? const CircularProgressIndicator()
+                : filterItems!.isEmpty
+                ? const Text('No se encontraron empleados')
                 : ListView.builder(
                     itemCount: filterItems?.length ?? 0,
                     itemBuilder: (BuildContext context, int index) {
