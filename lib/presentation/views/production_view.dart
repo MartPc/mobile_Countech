8336ed7eeb1a628e4baa2adf52e44e53fde7aabb
MartPc <mpinzonc07@gmail.com>
2024-03-20 11:20:15 -0500

    Modificaciones produccion

--- lib/presentation/views/production_view.dart
+++ lib/presentation/views/production_view.dart
@@ -65,7 +65,7 @@ class _ProductionViewState extends State<ProductionView> {
         appBar: AppBar(
         automaticallyImplyLeading: false,
         title: TextField(
-          decoration: InputDecoration(hintText: 'Buscar por orden de trabajo'),
+          decoration: const InputDecoration(hintText: 'Buscar por orden de trabajo'),
           onChanged: searchItem,
 
         ),
@@ -74,7 +74,7 @@ class _ProductionViewState extends State<ProductionView> {
           onRefresh: getData,
           child: Center(
             child: Container(
-              margin: EdgeInsets.all(10),
+              margin: const EdgeInsets.all(10),
               child:
                   filterItems == null
               ? const CircularProgressIndicator()
@@ -84,159 +84,155 @@ class _ProductionViewState extends State<ProductionView> {
                   itemCount: filterItems?.length ?? 0,
                   itemBuilder: (BuildContext context, int index) {
                     final data = filterItems![index];
-                    return Container(
-                      child: Accordion(
-                        headerBackgroundColor: Color.fromARGB(255, 255, 255, 255),
-                        headerBorderColor: Colors.grey,
-                        headerBorderWidth: 1,
-                        contentBorderColor: Colors.grey,
-                        contentBorderWidth: 1,
-                        paddingListTop: 0,
-                        paddingListBottom: 0,
-                        contentHorizontalPadding: 5,
-                        rightIcon: Icon(Icons.keyboard_arrow_down_rounded, size: 33, color: Colors.black,),
-                        headerPadding:
-                          const EdgeInsets.symmetric(vertical: 7, horizontal: 25),
-                        children: [
-                          AccordionSection(
-                              // leftIcon: Icon(Icons.circle_outlined, size: 20, color: Colors.black,),
-                              header: Container(
-                                  height: MediaQuery.of(context).size.height * 0.1,
-                                  child: Container(
-                                    child: Column(
-                                      mainAxisAlignment: MainAxisAlignment.center,
+                    return Accordion(
+                      headerBackgroundColor: const Color.fromARGB(255, 255, 255, 255),
+                      headerBorderColor: Colors.grey,
+                      headerBorderWidth: 1,
+                      contentBorderColor: Colors.grey,
+                      contentBorderWidth: 1,
+                      paddingListTop: 0,
+                      paddingListBottom: 0,
+                      contentHorizontalPadding: 5,
+                      rightIcon: const Icon(Icons.keyboard_arrow_down_rounded, size: 33, color: Colors.black,),
+                      headerPadding:
+                        const EdgeInsets.symmetric(vertical: 7, horizontal: 25),
+                      children: [
+                        AccordionSection(
+                            // leftIcon: Icon(Icons.circle_outlined, size: 20, color: Colors.black,),
+                            header: SizedBox(
+                                height: MediaQuery.of(context).size.height * 0.1,
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Column(
+                                      crossAxisAlignment: CrossAxisAlignment.start,
                                       children: [
-                                        Column(
-                                          crossAxisAlignment: CrossAxisAlignment.start,
-                                          children: [
-                                        Text(
-                                            'Orden de trabajo: ${data.ordenTrabajo}',
-                                            style: TextStyle(
-                                                color: Colors.black, fontSize: 15)),
-                                        Text(
-                                            'Cliente: ${data.cliente.nombreComercial}',
-                                            style: TextStyle(
-                                                color: Colors.black, fontSize: 15)),
-                                        Row(
-                                          children: [
-                                            Text(data.estado,
-                                                style: TextStyle(
-                                                    color: Colors.black,
-                                                    fontSize: 15, fontWeight: FontWeight.bold)), 
-                                            if (data.estado == 'En proceso')
-                                              Icon(Icons.hourglass_empty,
-                                                  color: Colors.blue, size: 20,),
-                                            if (data.estado == 'Registrado')
-                                              Icon(Icons.warning, color: Colors.orange, size: 20,)
-                                          ],
-                                        ),
-                                          ]
-                                        )
+                                    Text(
+                                        'Orden de trabajo: ${data.ordenTrabajo}',
+                                        style: const TextStyle(
+                                            color: Colors.black, fontSize: 15)),
+                                    Text(
+                                        'Cliente: ${data.cliente.nombreComercial}',
+                                        style: const TextStyle(
+                                            color: Colors.black, fontSize: 15)),
+                                    Row(
+                                      children: [
+                                        Text(data.estado,
+                                            style: const TextStyle(
+                                                color: Colors.black,
+                                                fontSize: 15, fontWeight: FontWeight.bold)), 
+                                        if (data.estado == 'En proceso')
+                                          const Icon(Icons.hourglass_empty,
+                                              color: Colors.blue, size: 20,),
+                                        if (data.estado == 'Registrado')
+                                          const Icon(Icons.warning, color: Colors.orange, size: 20,)
                                       ],
                                     ),
-                                  )),
-                              content: Container(
-                                  child: Container(
-                                      child: Column(
-                                children: [
-                                  Container(
-                                      child:
-                                          Text('Referencia: ${data.referencia}', 
-                                          style: TextStyle(
-                                            color: Colors.black,
-                                            fontSize: 17))),
-                                  SizedBox(
-                                    height: 5,
+                                      ]
+                                    )
+                                  ],
+                                )),
+                            content: Container(
+                                child: Container(
+                                    child: Column(
+                              children: [
+                                Container(
+                                    child:
+                                        Text('Referencia: ${data.referencia}', 
+                                        style: TextStyle(
+                                          color: Colors.black,
+                                          fontSize: 17))),
+                                SizedBox(
+                                  height: 5,
+                                ),
+                                Divider(),
+                                Container(
+                                  child: ListView.separated(
+                                    shrinkWrap: true,
+                                    itemCount:
+                                        data.procesoEnReferenciaEnPedidos.length,
+                                    separatorBuilder:
+                                        (BuildContext context, int index) =>
+                                            Divider(),
+                                    itemBuilder: (context, index) {
+                                      final proceso = data
+                                          .procesoEnReferenciaEnPedidos[index];
+                                      return Container(
+                                          child: ListTile(
+                                        title: Text(proceso.proceso,                   
+                                        style: TextStyle(
+                                          color: Colors.black,
+                                          fontSize: 17),
+                                        ),
+                                        subtitle: Row(
+                                          children: [
+                                            Text(getEstadoText(proceso.estado, proceso.cantidadAsignada),
+                                                style: TextStyle(
+                                                    color: Colors.black,
+                                                    fontSize: 16)),
+                                            if (proceso.estado == false &&
+                                                proceso.cantidadAsignada > 0)
+                                              Icon(Icons.hourglass_empty,
+                                                  color: Colors.blue, size: 20),
+                                            if (proceso.cantidadAsignada == 0)
+                                              Icon(Icons.warning,
+                                                  color: Colors.orange, size: 20),
+                                            if (proceso.estado == true)
+                                              Icon(Icons.check_circle,
+                                                  color: Colors.green, size: 20),
+                                          ],
+                                        ),
+                                        trailing: IconButton(
+                                          icon: Icon(Icons.info, color: Colors.blue, size: 30),
+                                          onPressed: () => showDialog<String>(
+                                            context: context,
+                                            builder: (BuildContext context) =>
+                                AlertDialog(
+                                  title: const Text(
+                                    'Detalle del proceso',
+                                    style: TextStyle(color: Colors.black, fontSize: 23),
+                                    textAlign: TextAlign.center,
                                   ),
-                                  Divider(),
-                                  Container(
-                                    child: ListView.separated(
-                                      shrinkWrap: true,
-                                      itemCount:
-                                          data.procesoEnReferenciaEnPedidos.length,
-                                      separatorBuilder:
-                                          (BuildContext context, int index) =>
-                                              Divider(),
-                                      itemBuilder: (context, index) {
-                                        final proceso = data
-                                            .procesoEnReferenciaEnPedidos[index];
-                                        return Container(
-                                            child: ListTile(
-                                          title: Text(proceso.proceso,                   
-                                          style: TextStyle(
-                                            color: Colors.black,
-                                            fontSize: 17),
-                                          ),
-                                          subtitle: Row(
-                                            children: [
-                                              Text(getEstadoText(proceso.estado, proceso.cantidadAsignada),
-                                                  style: TextStyle(
-                                                      color: Colors.black,
-                                                      fontSize: 16)),
-                                              if (proceso.estado == false &&
-                                                  proceso.cantidadAsignada > 0)
-                                                Icon(Icons.hourglass_empty,
-                                                    color: Colors.blue, size: 20),
-                                              if (proceso.cantidadAsignada == 0)
-                                                Icon(Icons.warning,
-                                                    color: Colors.orange, size: 20),
-                                              if (proceso.estado == true)
-                                                Icon(Icons.check_circle,
-                                                    color: Colors.green, size: 20),
-                                            ],
-                                          ),
-                                          trailing: IconButton(
-                                            icon: Icon(Icons.info, color: Colors.blue, size: 30),
-                                            onPressed: () => showDialog<String>(
-                                              context: context,
-                                              builder: (BuildContext context) =>
-            AlertDialog(
-              title: const Text(
-                'Detalle del proceso',
-                style: TextStyle(color: Colors.black, fontSize: 23),
-                textAlign: TextAlign.center,
-              ),
-              content: Column(
-                mainAxisSize: MainAxisSize.min,
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: <Widget>[
-                  Divider(),
-                  SingleChildScrollView(
-            child: ListBody(
-              children: <Widget>[
-                Text('Tipo de Máquina: ${proceso.tipoDeMaquina}', style: TextStyle(fontSize: 15, color: Colors.black)),
-                Text('Cantidad Total: ${proceso.cantidadTotal}', style: TextStyle(fontSize: 15, color: Colors.black) ),
-                Text('Cantidad Asignada: ${proceso.cantidadAsignada}', style: TextStyle(fontSize: 15, color: Colors.black) ),
-                Text('Cantidad Pendiente: ${proceso.cantidadPendiente}', style: TextStyle(fontSize: 15, color: Colors.black) ),
-                Text('Cantidad Terminada: ${proceso.cantidadHecha}', style: TextStyle(fontSize: 15, color: Colors.black) ),
-              ],
-            ),
-                  ),
-                  Divider(),
-                ],
-              ),
-              actions: <Widget>[
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 1.0),
-                  child: ElevatedButton(
-            onPressed: () => Navigator.pop(context, 'Cerrar'),
-            child: const Text('Cerrar', style: TextStyle(fontSize: 15, color: Colors.black)),
-                  ),
-                ),
-              ],
-            )
-                                            ),
-                                          ),
-                                        ));
-                                      },
+                                  content: Column(
+                                    mainAxisSize: MainAxisSize.min,
+                                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                                    children: <Widget>[
+                                      Divider(),
+                                      SingleChildScrollView(
+                                child: ListBody(
+                                  children: <Widget>[
+                                    Text('Tipo de Máquina: ${proceso.tipoDeMaquina}', style: TextStyle(fontSize: 15, color: Colors.black)),
+                                    Text('Cantidad Total: ${proceso.cantidadTotal}', style: TextStyle(fontSize: 15, color: Colors.black) ),
+                                    Text('Cantidad Asignada: ${proceso.cantidadAsignada}', style: TextStyle(fontSize: 15, color: Colors.black) ),
+                                    Text('Cantidad Pendiente: ${proceso.cantidadPendiente}', style: TextStyle(fontSize: 15, color: Colors.black) ),
+                                    Text('Cantidad Terminada: ${proceso.cantidadHecha}', style: TextStyle(fontSize: 15, color: Colors.black) ),
+                                  ],
+                                ),
+                                      ),
+                                      Divider(),
+                                    ],
+                                  ),
+                                  actions: <Widget>[
+                                    Padding(
+                                      padding: const EdgeInsets.symmetric(horizontal: 1.0),
+                                      child: ElevatedButton(
+                                onPressed: () => Navigator.pop(context, 'Cerrar'),
+                                child: const Text('Cerrar', style: TextStyle(fontSize: 15, color: Colors.black)),
+                                      ),
                                     ),
+                                  ],
+                                )
+                                          ),
+                                        ),
+                                      ));
+                                    },
                                   ),
-            
-                                  // Text(data.procesoEnReferenciaEnPedidos[0].proceso),
-                                ],
-                              ))))
-                        ],
-                      ),
+                                ),
+                                
+                                // Text(data.procesoEnReferenciaEnPedidos[0].proceso),
+                              ],
+                            ))))
+                      ],
                     );
                   },
                 ),
